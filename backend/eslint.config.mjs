a6d1d9ea9dff86ca1cdd254e9eedf0cc8930d955
Sadjoaldi <alsannealdi@hotmail.com>
2026-05-26 02:19:32 +0200

    fix(ci): frontend cache, backend e2e eslint allowDefaultProject

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -21,7 +21,7 @@ export default tseslint.config(
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
-        projet: ['./tsconfig.json'],
+        allowDefaultProject: ['test/*.ts'],
       },
     },
   },
